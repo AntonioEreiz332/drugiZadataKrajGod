2e3da68a54452938993cbd50e0b7dc7fa3b0a927
AntonioEreiz332 <58472078+AntonioEreiz332@users.noreply.github.com>
2021-05-31 09:09:02 +0200

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sekunde, minute;
+            Console.WriteLine("Upišite sekunde:");
+            sekunde = Convert.ToInt32(Console.ReadLine());
+            minute = sekunde / 60;
+            sekunde = sekunde - minute * 60;
+            Console.WriteLine(minute+"minute i "+sekunde+"sekundi!");
+
+
+            Console.ReadKey();
+        }
+    }
+}
